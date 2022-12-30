f00ad9bbeb7f2653e6dca7df84abf8986db009a2
hassanm <s1136140@student.hsleiden.nl>
2022-12-30 15:55:03 +0100

    initial commit

---
+++ src/test/java/com/example/backendwebshop/BackendWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
